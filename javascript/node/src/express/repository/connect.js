f1d36f8b099e1c9ad2b554f1cc12d5a417130162
seoyun-lee <20212025@o365.hanbat.ac.kr>
2023-03-27 00:07:10 +0900

    gitignore에 내용 수정

--- javascript/node/src/express/repository/connect.js
+++
@@ -1,8 +0,0 @@
-import {MongoClient} from "mongodb";
-
-export default async function connect() {
-  const databaseUrl = "mongodb://localhost:27017/";
-  const client = await MongoClient.connect(databaseUrl);
-  const database = client.db("sns");
-  return database.collection("user");
-}
